a985e23c022f0fa064c75a6cfdbbcae8bb92dcc5
s27674 <s27674@pjwstk.edu.pl>
2024-03-12 17:00:18 +0100

    Add GetAverage method

--- apbd1/apbd1/Program.cs
+++ apbd1/apbd1/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World1!");
 Console.WriteLine("Modyfikacja2!");
 Console.WriteLine("Modyfikacja3!");
+
+static double GetAverage(int[] nums)
+{
+    double sum = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        sum += nums[i];
+    }
+
+    return sum / nums.Length;
+}
